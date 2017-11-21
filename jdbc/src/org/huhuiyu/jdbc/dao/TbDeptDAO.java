ddfdc10b4d4e7876092c95907f1478e03ad5a76b
huhuiyu <1068306849@qq.com>
2017-11-21 13:16:19 +0800

    添加了部门查询功能

--- jdbc/src/org/huhuiyu/jdbc/dao/TbDeptDAO.java
+++ jdbc/src/org/huhuiyu/jdbc/dao/TbDeptDAO.java
@@ -3,21 +3,24 @@ package org.huhuiyu.jdbc.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.huhuiyu.jdbc.entity.TbDept;
 
 public class TbDeptDAO {
 	public static final String ADD = "insert into TbDept(deptName,deptInfo) values(?,?)";
+	public static final String QUERY = "select * from TbDept";
 
 	public static int addTbDept(TbDept dept) throws Exception {
 		Connection conn = Db.getConnection();
-		PreparedStatement ps = conn.prepareStatement(ADD,PreparedStatement.RETURN_GENERATED_KEYS);
+		PreparedStatement ps = conn.prepareStatement(ADD, PreparedStatement.RETURN_GENERATED_KEYS);
 		ps.setString(1, dept.getDeptName());
 		ps.setString(2, dept.getDeptInfo());
 		int result = ps.executeUpdate();
-		ResultSet rs=ps.getGeneratedKeys();
-		if(rs.next()) {
+		ResultSet rs = ps.getGeneratedKeys();
+		if (rs.next()) {
 			dept.setDeptId(rs.getInt(1));
 		}
 		rs.close();
@@ -26,12 +29,40 @@ public class TbDeptDAO {
 		return result;
 	}
 
-	public static void main(String[] args) throws Exception {
+	public static List<TbDept> queryAll() throws Exception {
+		List<TbDept> list = new ArrayList<TbDept>();
+		Connection conn = Db.getConnection();
+		PreparedStatement ps = conn.prepareStatement(QUERY);
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			TbDept dept = new TbDept();
+			dept.setDeptId(rs.getInt("deptId"));
+			dept.setDeptName(rs.getString("deptName"));
+			dept.setDeptInfo(rs.getString("deptInfo"));
+			dept.setCreated(rs.getTimestamp("created"));
+			list.add(dept);
+		}
+		return list;
+	}
+
+	public static void testAddTbDept() throws Exception {
 		Random random = new Random();
 		TbDept dept = new TbDept();
 		dept.setDeptName("部门" + random.nextInt());
 		dept.setDeptInfo("描述" + random.nextInt());
 		System.out.println("添加的结果：" + TbDeptDAO.addTbDept(dept));
 		System.out.println(dept);
+	}
+
+	public static void testQueryAll() throws Exception {
+		List<TbDept> list = TbDeptDAO.queryAll();
+		for (TbDept tbDept : list) {
+			System.out.println(tbDept);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		//TbDeptDAO.testAddTbDept();
+		TbDeptDAO.testQueryAll();
 	}
 }
